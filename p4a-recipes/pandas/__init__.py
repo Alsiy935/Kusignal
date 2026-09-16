1ec31147cef9506a2fb469cb253cd23c2c7cbf4e
Alsiy935 <dedypost123@gmail.com>
2026-09-16 20:38:55 +0200

    Update __init__.py

--- p4a-recipes/pandas/__init__.py
+++ p4a-recipes/pandas/__init__.py
@@ -1,35 +1,23 @@
-from os.path import join
-from pythonforandroid.recipe import MesonRecipe
+from pythonforandroid.recipe import PyProjectRecipe
 
 
-class PandasRecipe(MesonRecipe):
-    version = 'v2.3.3'
-    url = 'git+https://github.com/pandas-dev/pandas'
-    depends = ['numpy', 'libbz2', 'liblzma']
+class PandasRecipe(PyProjectRecipe):
+    version = "2.3.3"
+    url = "https://github.com/pandas-dev/pandas/releases/download/v{version}/pandas-{version}.tar.gz"
+
+    depends = [
+        "numpy",
+        "python-dateutil",
+        "pytz",
+    ]
+
     hostpython_prerequisites = [
         "versioneer[toml]",
         "numpy>=2.0",
-        "Cython<4.0.0a0"
+        "Cython>=3,<4",
+        "meson>=1.2.3,<2",
+        "meson-python>=0.15,<1",
     ]
-    patches = ['fix_numpy_includes.patch']
-    python_depends = ['python-dateutil', 'pytz']
-    need_stl_shared = True
-
-    def get_recipe_env(self, arch, **kwargs):
-        env = super().get_recipe_env(arch, **kwargs)
-
-        env['NUMPY_INCLUDES'] = join(
-            self.ctx.get_python_install_dir(arch.arch),
-            "numpy/_core/include",
-        )
-        env["PYTHON_INCLUDE_DIR"] = self.ctx.python_recipe.include_root(arch)
-        env['LDFLAGS'] += f' -landroid -l{self.stl_lib_name}'
-
-        return env
-
-    def build_arch(self, arch):
-        super().build_arch(arch)
-        self.restore_hostpython_prerequisites(["cython"])
 
 
 recipe = PandasRecipe()
